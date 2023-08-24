b5e757e6deebc425492dcc237dff3204194d02a0
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-08-24 16:42:32 +0200

    tag: connected signaling for new open clients

--- signals/connect.lua
+++ signals/connect.lua
@@ -3,11 +3,10 @@
 -- them when caught.
 ---
 
-local debug = require('utility.deubg')
 local log = require('utility.logging')
 
 --- Logs active tag into file.
--- File is locate in $HOME/.cache/awesomewm/<x>-active-tag
+-- File is located in $HOME/.cache/awesomewm/<x>-active-tag
 -- where <x> is the id of the display, where tag was
 -- selected.
 awesome.connect_signal('tag_selected', function(tag)
@@ -18,7 +17,17 @@ awesome.connect_signal('tag_selected', function(tag)
   log.log_to_file(path, tag.name)
 end)
 
---- Logs workspace overview into file.
-awesome.connect_signal('client_tagged', function(tag)
+require('utility.tag-overview')
 
+--- Logs workspace overview into JSON file.
+-- File is located in $HOME/.cache/awesomewm/tag-overview
+-- and contains overview (its display, id and number of
+-- clients) of all tags on every display managed by window
+-- manager, updated each time new client is open or moved
+-- to another tag.
+awesome.connect_signal('client_tagged', function(tag)
+  local json = get_workspaces()
+  local path = os.getenv('HOME') ..
+    '/.cache/awesomewm/tag-overview'
+  log.log_to_file(path, json)
 end)
