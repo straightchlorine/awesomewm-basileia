2fb1c11dc73718c860700f15859f9870c410583b
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-08-24 16:23:30 +0200

    tag: attached signals to each tag

--- configuration/tags.lua
+++ configuration/tags.lua
@@ -1,5 +1,7 @@
 local awful = require('awful')
 
+local signals = require('signals.tag-signals')
+
 -- default layouts
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
@@ -10,4 +12,5 @@ end)
 
 screen.connect_signal('request::desktop_decoration', function(s)
     awful.tag({ '1', '2', '3', '4','5','6' ,'7', '8', '9', '10'}, s, awful.layout.layouts[1])
+    signals.connect_signals(s.tags)
 end)
