51492044200f9919bc519916f3caddaa2a40bf5c
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-08-24 18:24:57 +0200

    startup: generates base files

--- configuration/startup/autorun.lua
+++ configuration/startup/autorun.lua
@@ -1,12 +1,13 @@
 local awful = require('awful')
 local gears = require('gears')
 
+local file_setup = require('utility.logging')
+
 local script_path = gears.filesystem.get_configuration_dir() .. 'configuration/scripts/'
 
 local function autorun ()
-
   awful.spawn.with_shell(script_path .. 'autorun')
-
+  file_setup.base_files()
 end
 
 autorun()
