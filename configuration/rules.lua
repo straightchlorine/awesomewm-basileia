304b5828d5d44c19f11cc385064da96bccb6f1f5
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-08-28 20:46:01 +0200

    fix: eww widgets are not managed by the wm

--- configuration/rules.lua
+++ configuration/rules.lua
@@ -1,42 +1,62 @@
 local ruled = require('ruled')
 local awful = require('awful')
 
-ruled.client.connect_signal("request::rules", function()
-    -- global rule
-    ruled.client.append_rule {
-        id         = "global",
-        rule       = { },
-        properties = {
-            size_hints_honor = false,
-            honor_workarea = true,
-            honor_padding = true,
-            raise     = true,
-            focus     = awful.client.focus.filter,
-            screen    = awful.screen.preferred,
-            placement = awful.placement.no_overlap+awful.placement.no_offscreen
-        }
+ruled.client.connect_signal('request::rules', function()
+  -- global rule
+  ruled.client.append_rule {
+    id         = 'global',
+    rule       = { },
+    properties = {
+      size_hints_honor = false,
+      honor_workarea = true,
+      honor_padding = true,
+      raise     = true,
+      focus     = awful.client.focus.filter,
+      screen    = awful.screen.preferred,
+      placement = awful.placement.no_overlap+awful.placement.no_offscreen
     }
+  }
 
-    -- floating clients
-    ruled.client.append_rule {
-        id       = "floating",
-        rule_any = {
-            instance = { "copyq", "pinentry" },
-            class    = {
-                "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
-                "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
-            },
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name    = {
-                "Event Tester",  -- xev.
-            },
-            role    = {
-                "AlarmWindow",    -- Thunderbird's calendar.
-                "ConfigManager",  -- Thunderbird's about:config.
-                "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
-            }
-        },
-        properties = { floating = true }
-    }
+  -- floating clients
+  ruled.client.append_rule {
+    id       = 'floating',
+    rule_any = {
+      instance = { 'copyq', 'pinentry' },
+      class    = {
+        'Arandr', 'Blueman-manager', 'Gpick', 'Kruler', 'Sxiv',
+        'Tor Browser', 'Wpa_gui', 'veromix', 'xtightvncviewer'
+      },
+      name    = {
+        'Event Tester',  -- xev.
+      },
+      role    = {
+        'AlarmWindow',    -- Thunderbird's calendar.
+        'ConfigManager',  -- Thunderbird's about:config.
+        'pop-up',         -- e.g. Google Chrome's (detached) Developer Tools.
+      }
+    },
+    properties = { floating = true }
+  }
+
+  -- eww widgets
+  ruled.client.append_rule({
+    rule_any = {
+      class = {
+        'eww',
+        'Eww'
+      },
+      name = {
+        'Eww - horizontal-bar',
+        'Eww - vertical-bar'
+      }
+    },
+    properties = {
+      valid = false,
+      focusable = false,
+      border_width = 0,
+      skip_taskbar = true,
+      skip_pager = true,
+      ontop = true
+    },
+  })
 end)
