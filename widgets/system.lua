c690a458b8850eb3d2d70895f7d6864fdc1f7ac8
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-20 15:34:20 +0200

    widget: cpu usage widget

--- widgets/system.lua
+++ widgets/system.lua
@@ -7,7 +7,22 @@ local awful = require('awful')
 
 local system = {}
 local ram_script = os.getenv('HOME') .. '/.config/widgets/scripts/ram_usage'
+local cpu_script = os.getenv('HOME') .. '/.config/widgets/scripts/cpu_usage'
 
+--- Return awful.widget object containing usage of cpu.
+function system.cpu()
+
+  local cpu = awful.widget.watch(cpu_script, 1, function (widget, stdout)
+    for line in stdout:gmatch('%d+%.%d+') do
+      widget:set_text('  ' .. line .. '%')
+    end
+    widget.forced_width = 80
+  end)
+
+  return cpu
+end
+
+--- Return awful.widget object containing usage of ram.
 function system.ram()
 
   local ram = awful.widget.watch(ram_script, 1, function (widget, stdout)
