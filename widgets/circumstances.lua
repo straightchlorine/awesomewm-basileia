29dedb24436d0454d0a987ec17db1625e28996e8
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-21 22:59:10 +0200

    widget: weather widget

--- widgets/circumstances.lua
+++ widgets/circumstances.lua
@@ -8,11 +8,22 @@ local wibox = require('wibox')
 
 local circumstances = {}
 
+-- path to the weather script
+local weather_script = os.getenv('HOME') .. '/.config/awesome/widgets/scripts/weather'
+
 function circumstances.time_and_date ()
 
   local time_and_date = wibox.widget.textclock('%A, %d %b %T', 1)
-  time_and_date.forced_width = 180
+  time_and_date.forced_width = 170
   return time_and_date
+
+end
+
+function circumstances.weather ()
+
+  local weather = awful.widget.watch(weather_script .. ' --prompt', 180)
+  weather.forced_width = 30
+  return weather
 
 end
 
