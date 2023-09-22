88407b7d6418b2f09e3bbc134b4c30319a3e872a
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-22 11:09:15 +0200

    widgets: merged time and date widget with weather one into single object

--- widgets/circumstances.lua
+++ widgets/circumstances.lua
@@ -11,19 +11,42 @@ local circumstances = {}
 -- path to the weather script
 local weather_script = os.getenv('HOME') .. '/.config/awesome/widgets/scripts/weather'
 
+--- Get awful.widget object displaying time and date.
+--
+-- @return awful.widget
 function circumstances.time_and_date ()
 
   local time_and_date = wibox.widget.textclock('%A, %d %b %T', 1)
-  time_and_date.forced_width = 170
+  time_and_date.forced_width = 160
   return time_and_date
 
 end
 
+--- Get awful.widget object displaying current weather.
+--
+-- @return awful.widget
 function circumstances.weather ()
 
   local weather = awful.widget.watch(weather_script .. ' --prompt', 180)
-  weather.forced_width = 30
+  weather.forced_width = 60
   return weather
+
+end
+
+--- Merges both time and weather widgets into single object.
+--
+-- @return awful.widget
+function circumstances.status()
+  return {
+    wibox.widget {
+      widget = wibox.widget.separator,
+      thickness = 0,
+      forced_width = 710
+    },
+    circumstances.time_and_date(),
+    circumstances.weather(),
+    layout = wibox.layout.align.horizontal,
+  }
 
 end
 
