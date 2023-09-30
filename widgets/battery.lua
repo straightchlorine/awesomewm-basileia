3e646358d699dcabb318826339c3ba5a48b2bd38
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-30 07:03:47 +0200

    widget: functino, which returns current battery capacity (in %)

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -21,4 +21,27 @@ function battery.exists()
   end)
 end
 
+--- Returns percentage of battery capacity.
+--
+-- Note: uses acpi utility.
+--
+-- @return int current capacity
+function battery.capacity()
+  local capacity = [=[
+    acpi -b | 
+    head -n 1 | 
+    sed 's/.*: //' | 
+    awk -F ',' '{print $2}' | 
+    tr -d ' ' | 
+    sed 's/%//'
+  ]=]
+
+  local capacity_percentage = nil
+  awful.spawn.easy_async_with_shell(capacity, function(out)
+    capacity_percentage = out
+  end)
+
+  return tonumber(capacity_percentage)
+end
+
 return battery
