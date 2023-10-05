f4e736017724575caeb36c88a9226157cab7f42c
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-05 16:42:42 +0200

    battery: removed redundant functions and cleaned up documentation

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -1,77 +1,14 @@
 --- battery.lua
--- Functions required for the battery widget.
+-- Auxiliary functions for the battery widget within widgets/system.lua
 ---
 
-local awful = require('awful')
 local battery = {}
 
---- Returns true if device is portable (i.e. has a battery).
+--- Returns icon for the battery, depending on both its capacity and status.
 --
--- Looks into /sys/class/power_supply folder, if empty device is assumed to not
--- be portable.
---
--- @return boolean
-function battery.exists()
-  awful.spawn.easy_async_with_shell('ls /sys/class/power_supply', function(out)
-    if out == nil then
-      return false
-    else
-      return true
-    end
-  end)
-end
-
---- Returns status of the battery.
---
--- Note: uses acpi utility.
---
--- @return int current capacity
-function battery.status()
-  local status = [=[
-    acpi -b |
-    head -n 1 |
-    sed 's/.*: //' |
-    awk -F ',' '{print $1}' |
-    tr -d ' ' |
-    sed 's/%//'
-  ]=]
-
-  local battery_status = nil
-  awful.spawn.easy_async_with_shell(status, function(out)
-    battery_status = out
-  end)
-
-  return battery_status
-end
-
---- Returns percentage of battery capacity.
---
--- Note: uses acpi utility.
---
--- @return int current capacity
-function battery.capacity()
-  local capacity = [=[
-    acpi -b |
-    head -n 1 |
-    sed 's/.*: //' |
-    awk -F ',' '{print $2}' |
-    tr -d ' ' |
-    sed 's/%//'
-  ]=]
-
-  local capacity_percentage = nil
-  awful.spawn.easy_async_with_shell(capacity, function(out)
-    capacity_percentage = out
-  end)
-
-  return tonumber(capacity_percentage)
-end
-
---- Returns icon for battery, depending on capacity and status.
---
--- @param capacity current capacity of the battery (in %)
--- @param status current status of the battery (for example Charging)
--- @return str icon
+-- @param capacity capacity of the battery (in %)
+-- @param status status of the battery (for example Charging or Discharging)
+-- @return chosen icon
 function battery.icon(capacity, status)
   if status == 'Charging' then
     if capacity == 100 then
