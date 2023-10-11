6293e78853ca589b6ced929dbb34e5f5d03b5eeb
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-11 19:44:20 +0200

    default-applications: tidied up and a bit of documenting
    Again, moved long, unreadable paths to utility.paths file.

--- configuration/default-applications.lua
+++ configuration/default-applications.lua
@@ -1,19 +1,17 @@
 --- default-applications.lua
--- returns array of commonly used applications
+-- Provides an object, containing applications which are often used.
+---
 
-local gears = require('gears')
-
-local scripts = gears.filesystem.get_configuration_dir() ..'configuration/scripts/'
-local rofi_applets = os.getenv('HOME') .. '/.config/rofi/applets/bin/'
+local paths = require('utility.paths')
 
 local defaults = {
   emulator = 'kitty',
   browser = 'firefox',
   music_player = 'kitty -e ncmpcpp',
-  launcher = os.getenv('HOME') .. '/.config/rofi/launchers/type-4/launcher.sh',
-  rotate_display = scripts .. 'toggle_rotation DisplayPort-0',
-  screenshot_utility = rofi_applets .. 'screenshot.sh',
-  powermenu = rofi_applets .. 'powermenu.sh',
+  launcher = paths.rofi,
+  rotate_display = paths.rotation,
+  screenshot_utility = paths.rofi_utils.screenshot_utility,
+  powermenu = paths.rofi_utils.powermenu,
   mpd_manager = {
     play_pause = 'mpc toggle',
     previous = 'mpc prev',
@@ -21,7 +19,7 @@ local defaults = {
     volume_up = 'mpc volume +2',
     volume_down = 'mpc volume -2',
     toggle_mute = 'pactl set-sink-mute @DEFAULT_SINK@ toggle',
-    mpd_rofi_popup = rofi_applets .. 'mpd.sh'
+    mpd_rofi_popup = paths.rofi_utils.mpd_controller
   }
 }
 
