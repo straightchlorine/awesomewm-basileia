eb98c18ac8acd4905af6a4a762b8e22409ba1fab
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-14 16:58:42 +0200

    tags: documenting and removing eww specific code

--- configuration/tags.lua
+++ configuration/tags.lua
@@ -1,23 +1,19 @@
 --- tags.lua
--- Defines layouts and tags themselves.
+-- Defines layouts and tags.
 ---
 
 local awful = require('awful')
 
--- default layouts
+-- setting default layouts
 tag.connect_signal("request::default_layouts", function()
   awful.layout.append_default_layouts({
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.tile.bottom,
+    awful.layout.suit.fair,
   })
 end)
 
-local signals = require('signals.tag-signals')
-
--- attach tags and layouts to each screen
+-- defining and attaching tags to each screen
 screen.connect_signal('request::desktop_decoration', function(s)
   awful.tag({ '1', '2', '3', '4','5','6' ,'7', '8', '9', '10'}, s, awful.layout.layouts[1])
-  signals.connect_signals(s.tags)
 end)
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
