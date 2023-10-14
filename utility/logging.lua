3943526f72c2c02756fae83605b7729a2e73b166
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-14 18:26:06 +0200

    logging: removed eww specific methods

--- utility/logging.lua
+++ utility/logging.lua
@@ -2,8 +2,7 @@
 -- Utility managing interactions between awesomewm
 -- and other external files.
 
-require('utility.paths')
-require('utility.tag-overview')
+local paths = require('utility.paths')
 local logger = {}
 
 function logger.log_to_file(file_path, content)
@@ -12,33 +11,6 @@ function logger.log_to_file(file_path, content)
     file:write(tostring(content))
     file:close()
   end
-end
-
-local function set_base_overview()
-  local content = get_workspaces()
-  local file = io.open(paths.tag_overview, 'w')
-
-  if file then
-    file:write(content)
-    file:close()
-  end
-end
-
-local function set_base_tags()
-  for i = 1, screen.count() do
-    local active_tag = tostring(i) .. '-active-tag'
-    local file = io.open(paths[active_tag], 'w')
-
-    if file then
-      file:write(tostring(1))
-      file:close()
-    end
-  end
-end
-
-function logger.base_files()
-  set_base_overview()
-  set_base_tags()
 end
 
 return logger
