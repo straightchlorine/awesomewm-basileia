eb49834f604f6d0f790acb75b6da6cb1e9025b21
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-15 17:17:16 +0200

    env: renamed paths.lua and added all possibly required hardcoded data
    Also removed paths.lua itself.

---
+++ utility/environment.lua
@@ -0,0 +1,76 @@
+--- environment.lua
+-- Contains important hardcoded data, such as paths and important variables.
+---
+
+local gears = require('gears')
+
+--- Object contains all preset paths and some variables, related to its execution
+local env = {}
+
+--- By default 'Mod4' i.e. windows key or 'Command'
+env.modkey = 'Mod4'
+
+--- Important data for the eww integration.
+env.eww = {}
+
+--- True, if eww integration is desired.
+env.eww.enable_integration = false
+
+local logs = os.getenv('HOME') .. '/.cache/awesomewm/'
+for i = 1, screen.count() do
+  local name = tostring(i) .. '-active-tag'
+  env.eww[name] = logs .. name
+end
+
+env.eww.tag_overview = logs .. 'tag-overview'
+
+local configuration_scripts = gears.filesystem.get_configuration_dir() .. 'configuration/scripts/' 
+
+--- Contains paths to scripts.
+env.scripts = {}
+
+--- Path to the autorun script.
+env.scripts.autorun = configuration_scripts .. 'autorun'
+
+-- Path to the screen responsible for display rotation.
+env.scripts.rotation = configuration_scripts .. 'toggle_rotation ' .. tostring(env.eww)
+
+local widget_scripts = gears.filesystem.get_configuration_dir() .. 'widgets/scripts/' 
+
+--- Contains paths to scripts used by system widgets.
+-- (widgets/system.lua)
+env.scripts.widgets = {}
+
+--- Path to cpu-usage script.
+env.scripts.widgets.cpu = widget_scripts .. 'cpu-usage'
+
+--- Path to ram-usage script.
+env.scripts.widgets.ram = widget_scripts .. 'ram-usage'
+
+--- Path to network-sample script.
+env.scripts.widgets.net = widget_scripts .. 'network-sample'
+
+--- Path to weather script.
+env.scripts.widgets.weather = widget_scripts .. 'weather --prompt'
+
+--- Paths related to rofi.
+env.rofi = {}
+
+--- Path to script launching rofi launcher.
+-- NOTE: If any other application launcher is used or rofi is launched
+-- as a command, putting the command itself in this variable also works.
+env.rofi.launcher = os.getenv('HOME') .. '/.config/rofi/launchers/type-4/launcher.sh'
+
+local rofi_applets = os.getenv('HOME') .. '/.config/rofi/applets/bin/'
+
+--- Path to rofi applet providing screenshot tool.
+env.rofi.screenshot_utility = rofi_applets .. 'screenshot.sh'
+
+--- Path to rofi applet providing powermenu.
+env.rofi.powermenu = rofi_applets .. 'powermenu.sh'
+
+--- Path to rofi applet providing mpd controler.
+env.rofi.mpd_controller = rofi_applets .. 'mpd.sh'
+
+return env
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
