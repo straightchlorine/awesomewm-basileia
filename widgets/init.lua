9d91516bba11d41f49c333810d431b6a138e64e9
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-15 17:20:52 +0200

    widgets/init: implemented environment.lua

--- widgets/init.lua
+++ widgets/init.lua
@@ -3,14 +3,14 @@
 ---
 
 local bars = require('widgets.bars')
-local paths = require('utility.paths')
+local env = require('utility.environment')
 
 --- Attaching appropirate bar to each screen
 screen.connect_signal('request::desktop_decoration', function(s)
   if s.index == 1 then
-    bars.main_bar(s, paths.modkey)
+    bars.main_bar(s, env.modkey)
   else
-    bars.side_bar(s, paths.modkey)
+    bars.side_bar(s, env.modkey)
   end
 end)
 -- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
