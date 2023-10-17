896026e5629ed97341cd0b3dc801a3e24414e0a1
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-17 11:52:32 +0200

    widgets/init: decides which set of widgets should be launched

--- widgets/init.lua
+++ widgets/init.lua
@@ -4,13 +4,26 @@
 
 local bars = require('widgets.bars')
 local env = require('utility.environment')
+local eww = require('utility.eww.integration')
 
---- Attaching appropirate bar to each screen
-screen.connect_signal('request::desktop_decoration', function(s)
-  if s.index == 1 then
-    bars.main_bar(s, env.modkey)
+--- Provides appropriate to the current configuration widgets.
+--
+-- If the eww integration is desired i.e. env.enable_integration is true, then
+-- eww widgets are provided. Otherwise, standard awesomewm widget configuration
+-- is utilised.
+local function widgets()
+  if  env.eww.enable_integration then
+    eww.setup()
   else
-    bars.side_bar(s, env.modkey)
+    screen.connect_signal('request::desktop_decoration', function(s)
+      if s.index == 1 then
+        bars.main_bar(s, env.modkey)
+      else
+        bars.side_bar(s, env.modkey)
+      end
+    end)
   end
-end)
+end
+
+widgets()
 -- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
