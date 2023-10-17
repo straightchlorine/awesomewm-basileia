38c97102d560f0ac385ea36abf922239afad4d79
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-17 11:48:44 +0200

    autorun: removed eww module completely

--- configuration/startup/autorun.lua
+++ configuration/startup/autorun.lua
@@ -4,14 +4,10 @@
 
 local awful = require('awful')
 local env = require('utility.environment')
-local eww = require('utility.eww-integration')
 
 --- Startup function.
 local function autorun ()
   awful.spawn.with_shell(env.scripts.autorun)
-
-  -- NOTE: runs only if variable env.eww is set to true  
-  eww.setup()
 end
 
 autorun()
