ff8fb96056b3cb15062e51ef4135711f11356387
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-30 19:46:58 +0100

    error-handler: slight change in documentation

--- configuration/startup/error-handler.lua
+++ configuration/startup/error-handler.lua
@@ -1,20 +1,20 @@
 --- error-handler.lua
--- Contains basic error handling (notification)
+-- Provides basic error handling for the window manager.
 ---
 
 local naughty = require('naughty')
 
---- If exception is caught, notification is displayed along with the
---- error message.
+--- If an error is caught, notification is displayed along with the message.
 local function error_handler()
   naughty.connect_signal('request::display_error', function(message, startup)
     naughty.notification {
       urgency = 'critical',
-      title = 'one must imagine Sisiphus happy' .. (startup and ' at the feet of the mountain...' or '...'),
+      title = 'One must imagine Sisiphus happy' .. (startup and ' at the feet of the mountain...' or '...'),
       message = message
     }
   end)
 end
 
 error_handler()
+
 -- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
