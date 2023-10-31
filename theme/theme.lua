391ad84c69bd62b8e9ff310e82a9f94fdf907ba6
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-31 12:18:23 +0100

    theme: increased useless_gap

--- theme/theme.lua
+++ theme/theme.lua
@@ -8,7 +8,7 @@ local theme = {}
 theme.fg_normal     = '#f5c7a8'
 theme.bg_normal     = '#102e3e'
 
-theme.useless_gap   = dpi(4)
+theme.useless_gap   = dpi(12)
 theme.border_width  = dpi(1)
 
 --- taglist coloring
