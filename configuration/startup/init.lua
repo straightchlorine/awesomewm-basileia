c24c2ffe4c3d5d195c701b30b941c7ac6e31fc76
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-06-17 21:08:20 +0200

    doc(startup/init): changed the comment a bit

--- configuration/startup/init.lua
+++ configuration/startup/init.lua
@@ -1,9 +1,9 @@
 --- startup/init.lua
--- Initializes startup module, calls both startup functions along with error
--- handling
+-- Initializes the window manager.
+-- (autorun along with error handling)
 ---
 
-require(... .. '.autorun')
-require(... .. '.error-handler')
+require(... .. ".autorun")
+require(... .. ".error-handler")
 
 -- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
