87e7eef3b841a22ec443cbe1906a986cc9918027
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-06-24 20:43:53 +0200

    doc(debug): added some docs for clarity

--- utility/debug.lua
+++ utility/debug.lua
@@ -1,7 +1,13 @@
+--- debug.lua
+-- Debugging utility, for quick troubleshooting.
+---
 local naughty = require 'naughty'
 
 local debug = {}
 
+--- Print quick notification with debug message.
+--
+-- @param msg Message to be displayed.
 function debug.notifyDebug(msg)
   naughty.notify {
     preset = naughty.config.presets.normal,
