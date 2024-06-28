2c80ea01a38f54ae613c23d48a29806b38e428b3
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-06-28 21:08:54 +0200

    chore(theme): added also example of a dark theme, provided by colorgen

--- configuration/startup/autorun.lua
+++ configuration/startup/autorun.lua
@@ -6,4 +6,5 @@ local awful = require 'awful'
 local env = require 'utility.environment'
 
 awful.spawn.with_shell(env.scripts.autorun)
+
 -- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
